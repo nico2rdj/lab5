145284990944b2a8e287730f4b651e5a1ecabbd8
Nicolas Martin <njm003@ucsd.edu>
2017-02-09 13:31:03 -0800

    final

--- routes/add.js
+++ routes/add.js
@@ -1,7 +1,14 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
-	var name = req.params.name;
-	console.log(name);
-	res.render('add');
- }
+exports.addFriend = function(req, res) { 
+	// Your code goes here
+
+        friend = {
+            "name":req.query.name,
+            "description": req.query.description,
+            "imageURL": "http://lorempixel.com/400/400/people"
+		};
+	console.log(friend);
+	data.friends.push(friend);
+    res.render('index', data);
+}
